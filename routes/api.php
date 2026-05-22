8de038d443d05af792a04758c0845e250f358d7e
TRESNAA <tresnaa@TRESNAAs-MacBook-Air.local>
2026-05-22 13:09:41 +0800

    Add full CRUD to subscription controller

--- routes/api.php
+++ routes/api.php
@@ -5,13 +5,10 @@ use App\Http\Controllers\Api\ServiceController;
 use App\Http\Controllers\Api\SubscriptionController;
 use App\Http\Controllers\Api\CustomerController;
 
-// Endpoint untuk Services
 Route::apiResource("services", ServiceController::class);
 Route::patch("services/{service}/activate", [ServiceController::class, "activate"]);
 Route::patch("services/{service}/deactivate", [ServiceController::class, "deactivate"]);
 
-// Endpoint untuk Subscriptions
 Route::apiResource("subscriptions", SubscriptionController::class);
 
-// Endpoint untuk Customers
 Route::apiResource("customers", CustomerController::class);
